eba964b72d7b952f60e20e316fa868a60b0cf799
JongRok-Lee <lrrghdrh@naver.com>
2024-05-23 06:20:11 +0000

    deprecate: ros2_numpy

--- tools/ros2_demo.py
+++ tools/ros2_demo.py
@@ -1,7 +1,6 @@
 import rclpy
 import rclpy.duration
 from rclpy.node import Node
-import ros2_numpy as rnp
 from sensor_msgs.msg import PointCloud2, PointField
 from std_msgs.msg import ColorRGBA
 from tf_transformations import quaternion_from_euler
@@ -13,6 +12,68 @@ from pathlib import Path
 from demo import DemoDataset, parse_config
 from pcdet.models import build_network, load_data_to_gpu
 from pcdet.utils import common_utils
+
+DUMMY_FIELD_PREFIX = '__'
+type_mappings = [(PointField.INT8, np.dtype('int8')),
+                 (PointField.UINT8, np.dtype('uint8')),
+                 (PointField.INT16, np.dtype('int16')),
+                 (PointField.UINT16, np.dtype('uint16')),
+                 (PointField.INT32, np.dtype('int32')),
+                 (PointField.UINT32, np.dtype('uint32')),
+                 (PointField.FLOAT32, np.dtype('float32')),
+                 (PointField.FLOAT64, np.dtype('float64'))]
+pftype_to_nptype = dict(type_mappings)
+
+def fields_to_dtype(fields, point_step):
+    '''Convert a list of PointFields to a numpy record datatype.
+    '''
+    offset = 0
+    np_dtype_list = []
+    for f in fields:
+        while offset < f.offset:
+            # might be extra padding between fields
+            np_dtype_list.append(
+                ('%s%d' % (DUMMY_FIELD_PREFIX, offset), np.uint8))
+            offset += 1
+
+        dtype = pftype_to_nptype[f.datatype]
+        if f.count != 1:
+            dtype = np.dtype((dtype, f.count))
+
+        np_dtype_list.append((f.name, dtype))
+        offset += pftype_to_nptype[f.datatype].itemsize * f.count
+
+    # might be extra padding between points
+    while offset < point_step:
+        np_dtype_list.append(('%s%d' % (DUMMY_FIELD_PREFIX, offset), np.uint8))
+        offset += 1
+
+    return np_dtype_list
+
+def pointcloud2_to_array(cloud_msg, squeeze=True):
+    ''' Converts a rospy PointCloud2 message to a numpy recordarray
+
+    Reshapes the returned array to have shape (height, width), even if the
+    height is 1.
+
+    The reason for using np.frombuffer rather than struct.unpack is
+    speed... especially for large point clouds, this will be <much> faster.
+    '''
+    # construct a numpy record type equivalent to the point type of this cloud
+    dtype_list = fields_to_dtype(cloud_msg.fields, cloud_msg.point_step)
+
+    # parse the cloud into an array
+    cloud_arr = np.frombuffer(cloud_msg.data, dtype_list)
+
+    # remove the dummy fields that were added
+    cloud_arr = cloud_arr[
+        [fname for fname, _type in dtype_list if not (
+            fname[:len(DUMMY_FIELD_PREFIX)] == DUMMY_FIELD_PREFIX)]]
+
+    if squeeze and cloud_msg.height == 1:
+        return np.reshape(cloud_arr, (cloud_msg.width,))
+    else:
+        return np.reshape(cloud_arr, (cloud_msg.height, cloud_msg.width))
 
 class CenterpointNode(Node):
     def __init__(self):
@@ -27,13 +88,13 @@ class CenterpointNode(Node):
         self.model.cuda()
         self.model.eval()
 
-        self.pc_sub = self.create_subscription(PointCloud2, '/lidar/top/pointcloud', self.pc_callback, 10)
-        self.vis_pub = self.create_publisher(MarkerArray, '/boxes', 10)
+        self.pc_sub = self.create_subscription(PointCloud2, '/lidar/top/pointcloud', self.pc_callback, 2)
+        self.vis_pub = self.create_publisher(MarkerArray, '/boxes', 2)
 
     def pc_callback(self, msg: PointCloud2):
         start = self.get_clock().now()
-        data = rnp.numpify(msg)
-        pc_ny = np.concatenate([data['xyz'], np.zeros([data['xyz'].shape[0], 1])], axis=1)
+        data = pointcloud2_to_array(msg)
+        pc_ny = np.vstack([data['x'], data['y'], data['z'], np.zeros(data['x'].shape[0])]).T
         input_dict = self.dataset.data_processor.forward(data_dict={'points': pc_ny,
                                                                     'use_lead_xyz': True})
         with torch.no_grad():
